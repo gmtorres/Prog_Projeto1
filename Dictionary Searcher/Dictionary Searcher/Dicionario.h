a367c46e3724466e3db3d84e3b44a99c645732e4
gmtorres <torresgustam@gmail.com>
2018-03-16 12:34:48 +0000

    GG, ez win

--- Dictionary Searcher/Dictionary Searcher/Dicionario.h
+++ Dictionary Searcher/Dictionary Searcher/Dicionario.h
@@ -3,19 +3,21 @@
 #include <iostream>
 #include <fstream>
 
-class Dicionario
-{
-public:
-	
-	std::string input_file_n,output_file_n;
+class Dicionario{
+private:
+	std::string input_file_n, output_file_n;
 	std::ifstream in_file;
 	std::ofstream out_file;
+
+
+public:
+	
+
 
 	Dicionario();  //Incializar o objeto sem parametros
 	Dicionario(std::string in_f, std::string out_f); //Inicializar o objeto com os nomes dos ficheiros de entrada e saida
 	void set_input(std::string str); // Atribuir um nome ao ficheiro de entrada
 	void set_output(std::string str); //Atribuir um nome ao ficheiro de saida
-
 	void read();
 
 	//~Dicionario();<
