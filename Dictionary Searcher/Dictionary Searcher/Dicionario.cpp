466b14996ceda752fbcb63eb6261a5016331a465
gmtorres <torresgustam@gmail.com>
2018-03-16 18:59:41 +0000

    TODO : (important )Handle special caracters, and split the string. Improvements on some functions and added otheres.

--- Dictionary Searcher/Dictionary Searcher/Dicionario.cpp
+++ Dictionary Searcher/Dictionary Searcher/Dicionario.cpp
@@ -23,20 +23,44 @@ void Dicionario::read() {
 		if (in_file.is_open()) {
 			std::string palavra;
 			while (in_file >> palavra) {
-				std::cout << palavra << std::endl;
+				//std::cout << palavra << std::endl;
+				if (isValid(palavra)) {
+					simple_words++;
+					if (!exists(palavra)) {
+						unique_words++;
+						words.push_back(palavra);
+					}
+				}
 			}
 		}
 		else {
 			std::cout << "Unable to open " <<input_file_n<< "." << std::endl;
 		}
 	}
-
 }
-bool Dicionario::VALIDA(std::string str) {
-	for (int i = 0; i < str.length(); i++)
-		if (islower(str[i]))
+bool Dicionario::isValid(std::string str) {
+	for (int i = 0; i < str.length(); i++) {
+		int c = int(str[i]);
+		if (c<65 || c>90)
 			return false;
+	}
 	return true;
+}
+void Dicionario::print() {
+	for (size_t i = 0; i < words.size(); i++)
+		std::cout << words.at(i) << std::endl;
+}
+bool Dicionario::exists(std::string str){
+	for (size_t i = 0; i < words.size(); i++)
+		if (str == words[i])
+			return true;
+	return false;
+}
+size_t  Dicionario::get_nSimple() {
+	return simple_words;
+}
+size_t Dicionario::get_nUnique() {
+	return unique_words;
 }
 
 
--- Dictionary Searcher/Dictionary Searcher/Dicionario.h
+++ Dictionary Searcher/Dictionary Searcher/Dicionario.h
@@ -3,14 +3,19 @@
 #include <stdio.h>
 #include <iostream>
 #include <fstream>
+#include <vector>
 
 class Dicionario{
 private:
 	std::string input_file_n, output_file_n;
 	std::ifstream in_file;
 	std::ofstream out_file;
+	std::vector<std::string> words;
+	size_t simple_words = 0;
+	size_t unique_words = 0;
 
-	bool VALIDA(std::string str);
+	bool isValid(std::string str);// verifica se uma palavra é valida, se nao contem lowercase letras
+	bool exists(std::string str); //verifica se uma palavra ja se encontra na lista, true se existir, falso caso não exista
 
 public:
 	
@@ -19,6 +24,8 @@ public:
 	void set_input(std::string str); // Atribuir um nome ao ficheiro de entrada
 	void set_output(std::string str); //Atribuir um nome ao ficheiro de saida
 	void read();
-
+	void print();
+	size_t get_nSimple();
+	size_t get_nUnique();
 };
 
